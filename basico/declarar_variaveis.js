e508be52640488be3d112a42da9587dba806231a
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2025-08-19 18:28:51 -0300

    Create declarar_variaveis.js

---
+++ basico/declarar_variaveis.js
@@ -0,0 +1,6 @@
+//Declarar variáveis (var, let, const) e imprimir no console.
+var idade = 28;
+let cidade = "São Paulo";
+const nome = "Maria";
+
+console.log(`A ${nome} tem ${idade} e mora em ${cidade}`);
